9977b24ad9ee42fb10470480453aa60a36461b04
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2020-01-23 18:53:30 +0530

    Form added and small HTML tweaks

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 # Create your views here.
 def homepage(request):
-    return render(request, template_name="todo_list/home.html")
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            allListItems = List.objects.all
+            messages.success(request, ('Item has been added to list!'))
+            return render(request, "todo_list/home.html", {'all_items': allListItems})
+    else:
+        allListItems = List.objects.all
+        return render(request, "todo_list/home.html", {'all_items':allListItems})
 
 def about(request):
     return render(request, "todo_list/about.html")
